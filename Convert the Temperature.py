8e1ad0912869324b68af8f9817c40a605a0ab5c2
SolyanaZewdie <129341297+SolyanaZewdie@users.noreply.github.com>
2025-09-16 20:44:04 +0300

    Create Convert the Temperature.py

---
+++ Convert the Temperature.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: list[float]
+        """
+        kelvin = celsius + 273.15
+        fahrenheit = celsius * 1.8 + 32.0
+        return [kelvin, fahrenheit]
+
+if __name__ == "__main__":
+  
+    celsius_input = float(input("Enter temperature in Celsius: "))
+    sol = Solution() 
+    result = sol.convertTemperature(celsius_input)
+    print("Kelvin", result[0]) 
+    print("Fahrenheit", result[1]) 
