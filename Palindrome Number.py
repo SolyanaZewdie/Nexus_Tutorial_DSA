08ab8ab0aca682f1ac60d79ab136196ff9a4580d
SolyanaZewdie <129341297+SolyanaZewdie@users.noreply.github.com>
2025-09-20 19:07:11 +0300

    Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,2 +1,13 @@
-def isPalindrome(self, x: int) -> bool:
-        return (str(x)== str(x)[::-1])
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        if x % 10 == 0 and x != 0:
+            return False
+
+        reverted = 0
+        while x > reverted:
+            reverted = reverted * 10 + x % 10
+            x //= 10
+
+        return x == reverted or x == reverted // 10
